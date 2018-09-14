6b51b540aa27971b1ba2b02435cf47ae70f84d20
thiago denny <thiago@debian.Home>
2018-09-14 19:53:56 -0300

    semana 06

---
+++ poo-aula/src/semana06/Cargo.java
@@ -0,0 +1,14 @@
+package semana06;
+
+public class Cargo {
+	public int codigo;
+	public String nome;
+	public double salarioMedio;
+	
+	public void mostrar() {
+		System.out.println(codigo);
+		System.out.println(nome);
+		System.out.println(salarioMedio);
+	}
+
+}
---
+++ poo-aula/src/semana06/Funcionario.java
@@ -0,0 +1,13 @@
+package semana06;
+
+public class Funcionario extends Pessoa{
+	
+	public int numCartao;
+	public Cargo cargo;
+	
+	public void mostrar() {
+		super.mostrar();
+		System.out.println(numCartao);
+		System.out.println(cargo.nome);
+	}
+}
