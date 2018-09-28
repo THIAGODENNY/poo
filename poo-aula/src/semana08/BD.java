5b1abfb346b0dec13e60de1f4b0b3a4e45283129
thiago denny <thiago@debian.Home>
2018-09-28 20:31:34 -0300

    semana 08

--- poo-aula/src/semana08/BD.java
+++ poo-aula/src/semana08/BD.java
@@ -1,7 +1,98 @@
 package semana08;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BD {
 	
+	public Connection connection;
+	public PreparedStatement preparedStatement;
+	public ResultSet resultSet;
 	
+	private final String DRIVER = 
+			"com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	
+	private final String BANCO = "brasil";
+	
+	private final String URL =
+			"jdbc:sqlserver://localhost:1433;databaseName="+BANCO;
+	
+	private final String LOGIN = "sa";
+	private final String SENHA = "tESTE1234";
+	
+	public boolean getConnection() {
 
+		try {
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(URL, LOGIN, SENHA);
+			return true;
+		} catch (ClassNotFoundException e) {
+			return false;
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+			return false;
+		}
+	}
+	
+	public void close() {
+		if(resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+			
+			if(preparedStatement != null) {
+				try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			try {
+				connection.close();
+				System.out.println("Desconectou");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		BD bd = new BD();
+		
+		bd.getConnection();
+		
+		try {
+			String sql = "select * from cidade";
+			bd.preparedStatement = bd.connection.prepareStatement(sql);
+			
+			bd.resultSet = bd.preparedStatement.executeQuery();
+			
+			int i = 1;
+			
+			while(bd.resultSet.next()) {
+				System.out.print(bd.resultSet.getString(1) + " , ");
+				System.out.print(bd.resultSet.getString(2) + " , ");
+				System.out.print(bd.resultSet.getString(3) + " , ");	
+				
+				System.out.println(i);
+				i++;
+				
+				System.out.println("-------------------------------");
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		bd.close();
+	}
+	
 }
